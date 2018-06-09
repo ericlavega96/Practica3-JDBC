11eb9c728c590392271ab7e12bc2f5aeadb50210
Eric Nunez <ericlavega96@gmail.com>
2018-06-09 18:06:19 -0400

    Subiendo plantilla Bootstrap funcionando con FreeMarker

---
+++ src/main/java/logical/Main.java
@@ -0,0 +1,30 @@
+package logical;
+
+import freemarker.template.Configuration;
+import spark.ModelAndView;
+import spark.template.freemarker.FreeMarkerEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+
+import static spark.Spark.staticFiles;
+
+public class Main {
+    public static void main(String[] args) {
+
+        staticFiles.location("/templates");
+
+        Configuration cfg = new Configuration(Configuration.VERSION_2_3_0);
+        cfg.setClassForTemplateLoading(Main.class, "/templates");
+        FreeMarkerEngine freeMarkerEngine = new FreeMarkerEngine(cfg);
+
+        get("/inicio", (request, response) -> {
+            Map<String, Object> attributes = new HashMap<>();
+            attributes.put("titulo", "Página de artículos A&E");
+            return new ModelAndView(attributes, "index.ftl");
+        }, freeMarkerEngine);
+    }
+}
