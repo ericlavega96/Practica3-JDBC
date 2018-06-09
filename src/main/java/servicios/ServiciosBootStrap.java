267531fd220689efbf6a04adff4a1ec6731877f3
AdonisVeloz <adonisveloz@hotmail.com>
2018-06-09 16:22:37 -0400

    Creación de servicios de base de datos y bootstrap.

---
+++ src/main/java/servicios/ServiciosBootStrap.java
@@ -0,0 +1,35 @@
+package servicios;
+
+import org.h2.tools.Server;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ServiciosBootStrap {
+
+    /**
+     *
+     * @throws SQLException
+     */
+    public static void startDb() throws SQLException {
+        Server.createTcpServer("-tcpPort", "9092", "-tcpAllowOthers").start();
+    }
+
+    /**
+     *
+     * @throws SQLException
+     */
+    public static void stopDb() throws SQLException {
+        Server.shutdownTcpServer("tcp://localhost:9092", "", true, true);
+    }
+
+
+    /**
+     * Metodo para recrear las tablas necesarios
+     * @throws SQLException
+     */
+    public static void crearTablas() throws  SQLException{
+
+    }
+}
---
+++ src/main/java/servicios/ServiciosDataBase.java
@@ -0,0 +1,27 @@
+package servicios;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ServiciosDataBase {
+    private static ServiciosDataBase instancia;
+    private String URL = "jdbc:h2:tcp://localhost/~/pruebaTep"; //Modo Server...
+
+    /**
+     *Implementando el patron Singleton
+     */
+
+    /**
+     * Retornando la instancia.
+     * @return
+     */
+    public static ServiciosDataBase getInstancia(){
+        if(instancia==null){
+            instancia = new ServiciosDataBase();
+        }
+        return instancia;
+    }
+}
