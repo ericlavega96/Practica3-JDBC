8b7f34e42cddfca2341dae28293ce5b5bc225506
AdonisVeloz <adonisveloz@hotmail.com>
2018-06-09 21:24:08 -0400

    Subiendo publicación de comentarios.

--- src/main/java/logical/Main.java
+++ src/main/java/logical/Main.java
@@ -4,8 +4,7 @@ import freemarker.template.Configuration;
 import spark.ModelAndView;
 import spark.template.freemarker.FreeMarkerEngine;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import static spark.Spark.get;
 import static spark.Spark.post;
@@ -21,6 +20,15 @@ public class Main {
         cfg.setClassForTemplateLoading(Main.class, "/templates");
         FreeMarkerEngine freeMarkerEngine = new FreeMarkerEngine(cfg);
 
+        Usuario user = new Usuario("aavgc","Adonis", "1234", false, false);
+        List<Articulo> articulos = new ArrayList<>();
+        List<Comentario> comentarios = new ArrayList<>();
+        Articulo articulo = new Articulo(1, "prueba", "prueba prueba prueba",user,new Date(),comentarios, null);
+        comentarios.add(new Comentario(1, "prueba prueba prueba", user, articulo));
+        comentarios.add(new Comentario(2, "probando uno dos tres", user, articulo));
+        articulos.add(articulo);
+
+
         get("/inicio", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
             attributes.put("titulo", "Página de artículos A&E");
@@ -29,9 +37,21 @@ public class Main {
 
         get("/post", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
+            attributes.put("articulo", articulo);
+            attributes.put("comentarios", articulo.getListaComentarios());
             attributes.put("titulo", "Artículos A&E - Post");
             return new ModelAndView(attributes, "post.ftl");
         }, freeMarkerEngine);
+
+        /*
+
+        <#list articulo.comentarios as comentario>
+                  <h5 class="mt-0">comentario.autor.nombre</h5>
+                    comentario.comentario
+                <#else>
+                  <h5 class="mt-0">No hay comentarios disponibles</h5>
+                </#list>
+         */
 
     }
 }
--- src/main/resources/templates/post.ftl
+++ src/main/resources/templates/post.ftl
@@ -111,13 +111,20 @@
           </div>
 
           <!-- Single Comment -->
-          <div class="media mb-4">
-            <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
-            <div class="media-body">
-              <h5 class="mt-0">Commenter Name</h5>
-              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-            </div>
-          </div>
+
+
+            <#list comentarios as comentario>
+              <div class="media mb-4">
+                  <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
+                  <div class="media-body">
+                  <h5 class="mt-0">${comentario.autor.username}</h5>
+                    ${comentario.comentario}
+                  </div>
+              </div>
+            </#list>
+
+
+
 
           <!-- Comment with nested comments -->
           <div class="media mb-4">
